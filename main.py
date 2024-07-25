4341d72698a040cbdcac135d090e43007f3aa556
thayr0ne <57337073+thayr0ne@users.noreply.github.com>
2024-07-25 00:30:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import time
@@ -22,7 +21,8 @@ async def read_powerbi_data():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    service = Service('/usr/bin/chromedriver')  # Caminho padrão do ChromeDriver no ambiente Vercel
+    chrome_options.binary_location = "/usr/bin/google-chrome"
+    service = Service("/usr/bin/chromedriver")
 
     driver = webdriver.Chrome(service=service, options=chrome_options)
     url = "https://app.powerbi.com/view?r=eyJrIjoiMDJmYTk1NTAtNGNjNi00MDM1LWFhMTgtNjBjNGM2M2VmMTgyIiwidCI6IjlkYmE0ODBjLTRmYTctNDJmNC1iYmEzLTBmYjEzNzVmYmU1ZiJ9"
@@ -32,14 +32,14 @@ async def read_powerbi_data():
     page_source = driver.page_source
     driver.quit()
 
-    soup = BeautifulSoup(page_source, 'html.parser')
-    tables = soup.find_all('table')
+    soup = BeautifulSoup(page_source, "html.parser")
+    tables = soup.find_all("table")
 
     data = []
     for table in tables:
         table_data = []
-        for row in table.find_all('tr'):
-            columns = row.find_all('td')
+        for row in table.find_all("tr"):
+            columns = row.find_all("td")
             row_data = [col.get_text(strip=True) for col in columns]
             table_data.append(row_data)
         data.append(table_data)
